39e9bc2ecc979a9c14ae92c61e0d70d9c82c2d0b
Vendrus <kayakowhacko@googlemail.com>
2018-12-15 17:35:01 +0000

    Started adding files and functionality for the UBC Launcher

---
+++ BestiaryLauncher/Model/ApplicationPaths.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestiaryLauncher.Model
+{
+    static class ApplicationPaths
+    {
+        public static string LauncherVersionFile = "launcherversion.txt";
+        public static string UbcVersionFile = "version.txt";
+
+        public static string RemoteLauncherVersionFile = "https://raw.githubusercontent.com/VendrusSci/UnofficialFRBestiaryCompanion/master/BestiaryLauncher/launcherversion.txt";;
+        public static string RemoteUbcVersionFile = "https://raw.githubusercontent.com/VendrusSci/UnofficialFRBestiaryCompanion/master/Bestiary/Resources/version.txt";
+
+        public static string GetDataDirectory()
+        {
+#if DEBUG
+            return "..\\..\\";
+#else
+            return Directory.GetCurrentDirectory();
+#endif
+        }
+
+        public static string GetResourcesDirectory()
+        {
+            return Path.Combine(GetDataDirectory(), "Resources");
+        }
+
+        public static string GetLauncherVersionPath()
+        {
+            return Path.Combine(GetDataDirectory(), LauncherVersionFile);
+        }
+
+        public static string GetUBCVersionPath()
+        {
+            return Path.Combine(GetResourcesDirectory(), UbcVersionFile);
+        }
+
+        public static string GetTempDirectory()
+        {
+            return Path.Combine(GetDataDirectory(), "Temp");
+        }
+        
+    }
+}
---
+++ BestiaryLauncher/Model/Updater.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestiaryLauncher.Model
+{
+    enum VersionType
+    {
+        LauncherVersion,
+        SoftwareVersion
+    }
+
+    class Updater
+    {
+        public Updater()
+        {
+            Directory.CreateDirectory(ApplicationPaths.GetTempDirectory());
+        }
+
+        private bool IsVersionDifferent(VersionType project)
+        {
+            bool result = false;
+
+            using (WebClient client = new WebClient())
+            {
+                switch (project)
+                {
+                    case VersionType.SoftwareVersion:
+                        try
+                        {
+                            client.DownloadFile(ApplicationPaths.RemoteUbcVersionFile, Path.Combine(ApplicationPaths.GetTempDirectory(), ApplicationPaths.UbcVersionFile));
+                        }
+                        catch(WebException ex)
+                        {
+                            //MainViewModel.UserActionLog.Error("Failed to download file");
+                        }
+                        string tempVersionFile = Path.Combine(ApplicationPaths.GetTempDirectory(), ApplicationPaths.UbcVersionFile);
+                        if (File.Exists(ApplicationPaths.GetUBCVersionPath()) && File.Exists(tempVersionFile))
+                        {
+                            if(File.ReadAllText(ApplicationPaths.GetUBCVersionPath()) == File.ReadAllText(tempVersionFile))
+                            {
+                                result = true;
+                            }
+                            else
+                            {
+                                result = false;
+                            }
+                        }
+                        break;
+                    case VersionType.LauncherVersion:
+                        try
+                        {
+                            client.DownloadFile(ApplicationPaths.RemoteLauncherVersionFile, Path.Combine(ApplicationPaths.GetTempDirectory(), ApplicationPaths.LauncherVersionFile));
+                        }
+                        catch (WebException ex)
+                        {
+                            //MainViewModel.UserActionLog.Error("Failed to download file");
+                        }
+                        string tempLauncherVersionFile = Path.Combine(ApplicationPaths.GetTempDirectory(), ApplicationPaths.LauncherVersionFile);
+                        if (File.Exists(ApplicationPaths.GetLauncherVersionPath()) && File.Exists(tempLauncherVersionFile))
+                        {
+                            if (File.ReadAllText(ApplicationPaths.GetLauncherVersionPath()) == File.ReadAllText(tempLauncherVersionFile))
+                            {
+                                result = true;
+                            }
+                            else
+                            {
+                                result = false;
+                            }
+                        }
+                        break;
+                }
+            }
+
+                
+
+            return result;
+        }
+
+        private bool FamiliarUpdateAvailable()
+        {
+            bool result = false;
+
+
+
+
+            return result;
+        }
+    }
+}
