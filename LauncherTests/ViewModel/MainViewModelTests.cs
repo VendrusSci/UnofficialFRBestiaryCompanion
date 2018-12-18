47a16d0511289837811812f7259db8ab43c6f068
Vendrus <kayakowhacko@googlemail.com>
2018-12-18 08:05:29 +0000

    Finished unit tests for MainViewModel

--- LauncherTests/ViewModel/MainViewModelTests.cs
+++ LauncherTests/ViewModel/MainViewModelTests.cs
@@ -2,6 +2,7 @@
 using BestiaryLauncher.ViewModels;
 using NSubstitute;
 using NUnit.Framework;
+using System.IO;
 using System.Text;
 
 namespace LauncherTests.ViewModel
@@ -66,6 +67,90 @@ namespace LauncherTests.ViewModel
             directoryManipulator.Exists(Arg.Any<string>()).Returns(false);
             MainViewModel viewModel = new MainViewModel(loader, downloader, unzipper, fileManipulator, directoryManipulator, processStarter, applicationCloser);
             var isAvailable = viewModel.UpdateLauncher.CanExecute(null);
+            Assert.IsFalse(isAvailable, "Update should not be available");
+        }
+
+        [Test]
+        public void TestUpdateSoftwareExecuteWithSuccess()
+        {
+            directoryManipulator.Exists(Arg.Any<string>()).Returns(false);
+            MainViewModel viewModel = new MainViewModel(loader, downloader, unzipper, fileManipulator, directoryManipulator, processStarter, applicationCloser);
+            viewModel.UpdateSoftware.Execute(null);
+            Assert.IsTrue(viewModel.UpdateStatusText == "Update Successful", "Update should have completed");
+        }
+
+        [Test]
+        public void TestUpdateSoftwareExecuteWithFailure()
+        {
+            downloader.Download(Arg.Any<string>()).Returns(_ => null);
+            directoryManipulator.Exists(Arg.Any<string>()).Returns(false);
+            MainViewModel viewModel = new MainViewModel(loader, downloader, unzipper, fileManipulator, directoryManipulator, processStarter, applicationCloser);
+            viewModel.UpdateSoftware.Execute(null);
+            Assert.IsTrue(viewModel.UpdateStatusText == "Update Failed, check connection or report bug", "Update should have completed");
+        }
+
+        [Test]
+        public void TestUpdateSoftwareOnExecuteWithUpdateAvailable()
+        {
+            loader.Load(Path.Combine(ApplicationPaths.GetDataDirectory(), ApplicationPaths.UbcExeFile)).Returns(Encoding.ASCII.GetBytes("this is old"));
+            downloader.Download(Arg.Any<string>()).Returns(Encoding.ASCII.GetBytes("this is new"));
+            directoryManipulator.Exists(Arg.Any<string>()).Returns(false);
+            MainViewModel viewModel = new MainViewModel(loader, downloader, unzipper, fileManipulator, directoryManipulator, processStarter, applicationCloser);
+            var isAvailable = viewModel.UpdateSoftware.CanExecute(null);
+            Assert.IsTrue(isAvailable, "Update should be available");
+        }
+
+        [Test]
+        public void TestUpdateSoftwareOnExecuteWithNoUpdateAvailable()
+        {
+            loader.Load(Path.Combine(ApplicationPaths.GetDataDirectory(), ApplicationPaths.UbcExeFile))
+                .Returns(Encoding.ASCII.GetBytes("this is old"));
+            downloader.Download(Arg.Any<string>()).Returns(Encoding.ASCII.GetBytes("this is old"));
+            directoryManipulator.Exists(Arg.Any<string>()).Returns(false);
+            MainViewModel viewModel = new MainViewModel(loader, downloader, unzipper, fileManipulator, directoryManipulator, processStarter, applicationCloser);
+            var isAvailable = viewModel.UpdateSoftware.CanExecute(null);
+            Assert.IsFalse(isAvailable, "Update should not be available");
+        }
+
+        [Test]
+        public void TestUpdateFamiliarsExecuteWithSuccess()
+        {
+            directoryManipulator.Exists(Arg.Any<string>()).Returns(false);
+            MainViewModel viewModel = new MainViewModel(loader, downloader, unzipper, fileManipulator, directoryManipulator, processStarter, applicationCloser);
+            viewModel.UpdateFamiliars.Execute(null);
+            Assert.IsTrue(viewModel.UpdateStatusText == "Update Successful", "Update should have completed");
+        }
+
+        [Test]
+        public void TestUpdateFamiliarsExecuteWithFailure()
+        {
+            downloader.Download(Arg.Any<string>()).Returns(_ => null);
+            directoryManipulator.Exists(Arg.Any<string>()).Returns(false);
+            MainViewModel viewModel = new MainViewModel(loader, downloader, unzipper, fileManipulator, directoryManipulator, processStarter, applicationCloser);
+            viewModel.UpdateFamiliars.Execute(null);
+            Assert.IsTrue(viewModel.UpdateStatusText == "Update Failed, check connection or report bug", "Update should have completed");
+        }
+
+        [Test]
+        public void TestUpdateFamiliarsOnExecuteWithUpdateAvailable()
+        {
+            loader.Load(Path.Combine(ApplicationPaths.GetResourcesDirectory(), ApplicationPaths.FRDataFile)).Returns(Encoding.ASCII.GetBytes("this is old"));
+            downloader.Download(Arg.Any<string>()).Returns(Encoding.ASCII.GetBytes("this is new"));
+            directoryManipulator.Exists(Arg.Any<string>()).Returns(false);
+            MainViewModel viewModel = new MainViewModel(loader, downloader, unzipper, fileManipulator, directoryManipulator, processStarter, applicationCloser);
+            var isAvailable = viewModel.UpdateFamiliars.CanExecute(null);
+            Assert.IsTrue(isAvailable, "Update should be available");
+        }
+
+        [Test]
+        public void TestUpdateFamiliarsOnExecuteWithNoUpdateAvailable()
+        {
+            loader.Load(Path.Combine(ApplicationPaths.GetResourcesDirectory(), ApplicationPaths.FRDataFile))
+                .Returns(Encoding.ASCII.GetBytes("this is old"));
+            downloader.Download(Arg.Any<string>()).Returns(Encoding.ASCII.GetBytes("this is old"));
+            directoryManipulator.Exists(Arg.Any<string>()).Returns(false);
+            MainViewModel viewModel = new MainViewModel(loader, downloader, unzipper, fileManipulator, directoryManipulator, processStarter, applicationCloser);
+            var isAvailable = viewModel.UpdateFamiliars.CanExecute(null);
             Assert.IsFalse(isAvailable, "Update should not be available");
         }
     }
