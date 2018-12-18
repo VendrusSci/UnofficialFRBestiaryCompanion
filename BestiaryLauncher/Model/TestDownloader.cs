1d748b042a225339c9d2438959ba7c6c2b84170d
Vendrus <kayakowhacko@googlemail.com>
2018-12-18 21:55:06 +0000

    Added test downloader for testing update without going online

---
+++ BestiaryLauncher/Model/TestDownloader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestiaryLauncher.Model
+{
+    class TestDownloader : IDownloadFiles
+    {
+        private string basePath = Path.Combine("..\\..\\..\\", "BestiaryTestData");
+
+        public byte[] Download(string url)
+        {
+            var fakePath = Path.Combine(basePath, Path.GetFileName(url));
+            return File.ReadAllBytes(fakePath);
+        }
+    }
+}
